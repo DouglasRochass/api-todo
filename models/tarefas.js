804db2b50a6bdae78fccab885f581a71b75ede71
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2024-02-01 14:55:37 -0300

    Add files via upload

--- models/tarefas.js
+++ models/tarefas.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 const { format, parse } = require('date-fns');
 
 const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  priority: { type: String, required: true },
+  titulo: { type: String, required: true },
+  descricao: { type: String },
+  prioridade: { type: String, required: true },
+  categoria: {type: String, required: true},
   data: {
     type: Date,
     required: true,
   },
-  completed: { type: Boolean, default: false },
+  completado: { type: Boolean, default: false },
+
 });
 
-const Task = mongoose.model('Task', taskSchema);
+const lista = mongoose.model('Task', taskSchema);
 
-module.exports = Task;
+module.exports = lista;
